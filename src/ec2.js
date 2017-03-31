5a32d8a8d71a6f8bdc336baf5ae51645c3565840
leontin26 <leontin26@yahoo.com>
2017-03-31 19:57:21 +0300

    Inpartire functii

--- src/ec2.js
+++ src/ec2.js
@@ -1,25 +1,62 @@
-function rezolva()
-{
+function citeste(){
 	var a = document.getElementById("a").value;
 	var b = document.getElementById("b").value;
 	var c = document.getElementById("c").value;
 	
-	var delta = b*b - 4*a*c;
-	if (delta >= 0) {
-		var x1_re = (-b - Math.sqrt(delta)) / (2 * a);
-		var x1_im = 0;
-		var x2_re = (-b - Math.sqrt(delta)) / (2 * a);
-		var x2_im = 0;
-		document.getElementById("x1").innerHTML = x1_re;
-		document.getElementById("x2").innerHTML = x2_re;
+	var coef = [a, b, c];
+	
+	return coef;
+}
+
+// -----------------------------------------------------
+
+function calcul_delta(coeficient){
+	return coeficient[1] * coeficient[1] - 4 * coeficient[0] * coeficient[2];
+}
+
+
+// -----------------------------------------------------
+
+function calcul_radacini(coef, delta){
+var x1_re;
+var x1_im;
+var x2_re;
+var x2_im; 
+if (delta >= 0) {
+		 x1_re = (-coef[1] - Math.sqrt(delta)) / (2 * coef[0]);
+		 x1_im = 0;
+		 x2_re = (-coef[1] - Math.sqrt(delta)) / (2 * coef[0]);
+		 x2_im = 0;
 	}
 	else {
-		var x1_re = (-b) / (2 * a);
-		var x1_im = - Math.sqrt(-delta) / (2 * a);
-		var x2_re = (-b ) / (2 * a);
-		var x2_im = + Math.sqrt(-delta) / (2 * a);
-		document.getElementById("x1").innerHTML = x1_re.toString() + " + " + x1_im.toString() + " i";
-		document.getElementById("x2").innerHTML = x2_re.toString() + " + " + x2_im.toString() + " i";
+		 x1_re = (-coef[1]) / (2 * coef[0]);
+		 x1_im = - Math.sqrt(-delta) / (2 * coef[0]);
+		 x2_re = (-coef[1] ) / (2 * coef[0]);
+		 x2_im = + Math.sqrt(-delta) / (2 * coef[0]);
 	}
+return [x1_re, x1_im, x2_re, x2_im];
+}
+// -----------------------------------------------------
+function afiseaza(radacini){
+	if (radacini[1] >= 0)
+		document.getElementById("x1").innerHTML = radacini[0].toString() + " + " + radacini[1].toString() + " i";
+	else
+		document.getElementById("x1").innerHTML = radacini[0].toString() + radacini[1].toString() + " i";
+	if (radacini[3] >= 0)
+		document.getElementById("x2").innerHTML = radacini[2].toString() + " + " + radacini[3].toString() + " i";
+	else
+		document.getElementById("x2").innerHTML = radacini[2].toString() + radacini[3].toString() + " i";
+}
+
+// -----------------------------------------------------
+function rezolva()
+{
+	
+	var coef = citeste();
+	var delta = calcul_delta(coef);
+	var radacini = calcul_radacini(coef, delta);
+	afiseaza(radacini);
 	
 }
+
+
