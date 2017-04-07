3f19f815f69d748c9b4a52e930f9986c183a9a21
leontin26 <leontin26@yahoo.com>
2017-04-07 18:44:21 +0300

    Creare rect.js, modificat senzor.html
    Desenat dreptunghi alpha

--- src/senzor/rect.js
+++ src/senzor/rect.js
@@ -13,13 +13,19 @@ function on_device_orientation(evt)
 	
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
+	ctx.setTransform(1, 0, 0, 1, 0, 0);
 	ctx.clearRect(0, 0, canvas.height, canvas.width);
 	
 	var raza = 40;
 	var latura_patrat = 50;
 	var centru = {x:canvas.width/2, y:canvas.height/2};
+	
+//	ctx.arc(centru.x + gamma * (canvas.width/2 - raza)/90, centru.y + beta * (canvas.height/2 - raza)/90, raza, 0, 2 * Math.PI);
+	
+	ctx.translate(centru.x, centru.y);
+	ctx.rotate(alpha * Math.PI/180);
+	
 	ctx.beginPath();
-	ctx.arc(centru.x + gamma * (canvas.width/2 - raza)/90, centru.y + beta * (canvas.height/2 - raza)/90, raza, 0, 2 * Math.PI);
-
+	ctx.rect(-latura_patrat/2, -latura_patrat/2, latura_patrat*2, latura_patrat/2);
 	ctx.stroke();
 }
