9f52571f11f3e3a1b5b2831380b2d69fca3ecb96
leontin26 <leontin26@yahoo.com>
2017-04-07 17:16:05 +0300

    Modificare, corectare program

--- src/senzor/senzor.js
+++ src/senzor/senzor.js
@@ -11,4 +11,13 @@ function on_device_orientation(evt)
 	document.getElementById("b").innerHTML = "beta = " + beta.toString();
 	document.getElementById("g").innerHTML = "gamma = " + gamma.toString();
 	
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, canvas.height, canvas.width);
+	
+	var raza = 40;
+	var centru = {x:canvas.width/2, y:canvas.height/2};
+	ctx.beginPath();
+	ctx.arc(centru.x + gamma * (canvas.width/2 - raza)/90, centru.y + beta * (canvas.height/2 - raza)/90, raza, 0, 2 * Math.PI);
+	ctx.stroke();
 }
