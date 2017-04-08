076fbc6ad94471a12f9f7176cab8536c6316259c
leontin26 <leontin26@yahoo.com>
2017-04-09 01:04:36 +0300

    Touch - corectare

--- src/senzor/touch.js
+++ src/senzor/touch.js
@@ -1,21 +1,21 @@
+
 window.addEventListener("touchstart", on_touchstart);
 
-var touch_colors = ["#ff0000", "#00ff00", "#0000ff", "#0f0f00"];
+var touch_colors = ["#FF0000", "#00FF00", "#0000FF", "#0F0F00"];
  
 function on_touchstart(evt)
 {
 	var touches = evt.changedTouches;
 	
 	var canvas = document.getElementById("canvas");
-	
+	 
 	var ctx = canvas.getContext("2d");
 	
-	for (var i = 0; i < touches.length; i++)
+	for (var i = 0; i < touches.length; i=i+1)
 	{
 		ctx.beginPath();
-		ctx.strokeStyle = touch_color[i];
+		ctx.strokeStyle = touch_colors[i];
 		ctx.arc(touches[i].pageX, touches[i].pageY, 10, 0, 2 * Math.PI);
 		ctx.stroke();
 	}
-	
 }
