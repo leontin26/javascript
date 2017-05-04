529c22bf682aed637d2dfb26f40bca8874426512
leontin26 <leontin26@yahoo.com>
2017-05-04 20:02:28 +0300

    svg3 color

--- src/senzor/svg3.js
+++ src/senzor/svg3.js
@@ -1,18 +1,34 @@
  var svg = document.getElementById("svg");
  svg.addEventListener("touchstart", on_touch_start);
  
+ function random_color()
+ {
+	 var s = "0123456789ABCDEF";
+	 var c = "#";
+	 for (var i = 0; i < 6; i++)
+	 {
+		var j = Math.floor(Math.random() * 15);
+		c += s[j];
+	
+	 }
+	 	return c;
+	 
+ }
+ 
+ 
  function on_touch_start(evt)
  {
 	for (var i = 0; i < evt.touches.length; i++)
 	{
+		var color = random_color();
 		var t = evt.touches[i];
 		var c = document.createElementNS("http://www.w3.org/2000/svg","ellipse");
 		c.setAttribute("cx", t.pageX);
 		c.setAttribute("cy", t.pageY);
 		c.setAttribute("rx", 20);
-		c.setAttribute("ry", 40);
-		c.setAttribute("stroke", "green");
-		c.setAttribute("fill", "green");
+		c.setAttribute("ry", 10);
+		c.setAttribute("stroke", color);
+		c.setAttribute("fill", color);
 		svg.appendChild(c);
 	} 
 	 
