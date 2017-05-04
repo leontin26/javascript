62978832b99e28fba628c2e408abc0ca5e54b9ed
leontin26 <leontin26@yahoo.com>
2017-05-04 19:12:24 +0300

    svg 2 cercuri

--- src/senzor/svg.js
+++ src/senzor/svg.js
@@ -1,11 +1,17 @@
  
  var c1 = document.getElementById("cerc1");
+ var c2 = document.getElementById("cerc2");
  
  c1.addEventListener("touchmove", on_touch_move);
+ c2.addEventListener("touchmove", on_touch_move);
  
  function on_touch_move(e)
  {
-	var t = e.changedTouches[0];
-	c1.setAttribute("cx", t.pageX);
-	c1.setAttribute("cy", t.pageY);
+	for (var i = 0; i < e.changedTouches.length; i++)
+	{
+		var t = e.changedTouches[i];
+		var c = t.target;
+		c.setAttribute("cx", t.pageX);
+		c.setAttribute("cy", t.pageY);
+	}
  }
