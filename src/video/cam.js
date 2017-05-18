b1f3dbe383d0f073ef7ea7de9a9043d81684c7b1
leontin26 <leontin26@yahoo.com>
2017-05-18 19:44:45 +0300

    camera spate

--- src/video/cam.js
+++ src/video/cam.js
@@ -1,6 +1,6 @@
 
 var video = document.getElementById("video");
-
+video.addEventListener("touchstart", capteaza);
 
 function on_cam_success(stream) 
 {
@@ -19,8 +19,25 @@ function on_cam_err(err)
   
 //------------------------------------------------------------------
 
-var constraints = { audio: true, video: true };
+var constraints = { audio: true, video: { facingMode: "environment" }};
 
 navigator.mediaDevices.getUserMedia(constraints)
 .then(on_cam_success)
 .catch(on_cam_err);
+
+//------------------------------------------------------------------
+
+function capteaza()
+{
+	var c = document.getElementById("canvas");
+	c.width = video.width;
+	c.height = video.height;
+	
+	var ctx = c.getContext("2d");
+	
+	ctx.drawImage(video, 0 ,0, 640, 480);
+}
+
+//------------------------------------------------------------------
+
+
